e1d204f7431bb6938514bf6d5a3ab5361c01c38a
yusayusa <yusakzk@gmail.com>
2016-10-10 21:14:40 +0900

    initial commit

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by KazukiYusa on 2016/10/10.
+//  Copyright © 2016年 KazukiYusa. All rights reserved.
+//
+
+import UIKit
+
+import HighlightTextView
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HighlightTextView/HighlightTextView.h
@@ -0,0 +1,19 @@
+//
+//  HighlightTextView.h
+//  HighlightTextView
+//
+//  Created by KazukiYusa on 2016/10/10.
+//  Copyright © 2016年 KazukiYusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HighlightTextView.
+FOUNDATION_EXPORT double HighlightTextViewVersionNumber;
+
+//! Project version string for HighlightTextView.
+FOUNDATION_EXPORT const unsigned char HighlightTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HighlightTextView/PublicHeader.h>
+
+
